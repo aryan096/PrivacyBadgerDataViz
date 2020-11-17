bd1489ccc4009126eb1e1ffad98d3ee552090574
ragnaag <45662914+ragnaag@users.noreply.github.com>
2020-11-16 21:32:21 -0500

    Add files via upload

---
+++ src/Viz.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {withRouter} from 'react-router';
+
+function Viz() {
+
+     return (
+       <div>
+         <p>This is the second page.</p>
+       </div>
+     );
+
+ }
+
+ export default Viz;
