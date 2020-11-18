71f2904f596f469deb9b31520bb6a89e1c495964
Aryan Srivastava <asriva11@mario>
2020-11-18 17:54:21 -0500

    total_num_trackers function

--- src/Viz.js
+++ src/Viz.js
@@ -7,41 +7,50 @@ const social = data.snitch_map;
 
 class Viz extends Component{
 
-
   get_top_trackers() {
+    var snitches = {};
 
-  var snitches = {};
+    // Variable for Top __ websites with trackers on different websites
+    var top_num = 10
 
-  // Variable for Top __ websites with trackers on different websites
-  var top_num = 10
-
-  for (let tracker in data['snitch_map']) {
-    var websites = data['snitch_map'][tracker];
-    for (var i = 0; i < websites.length; i++) {
-      if (websites[i] in snitches){
-        snitches[websites[i]] += 1
-      } else {
-        snitches[websites[i]] = 1;
+    for (let tracker in data['snitch_map']) {
+      var websites = data['snitch_map'][tracker];
+      for (var i = 0; i < websites.length; i++) {
+        if (websites[i] in snitches){
+          snitches[websites[i]] += 1
+        } else {
+          snitches[websites[i]] = 1;
+        }
       }
     }
+
+    // Create items array
+    var items = Object.keys(snitches).map(function(key) {
+      return [key, snitches[key]];
+    });
+
+    // Sort the array based on the second element
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    });
+
+    // Create a new array with only the first top_num items
+    var sorted_snitches_top = items.slice(0, top_num)
+
+    return sorted_snitches_top;
   }
 
-  // Create items array
-  var items = Object.keys(snitches).map(function(key) {
-    return [key, snitches[key]];
-  });
+  get_total_num_trackers() { 
+    // This function returns the total number of trackers based on the info stored in the snitch_map
+    // each key in the snitch map refers to the top level domain of a tracker
 
-  // Sort the array based on the second element
-  items.sort(function(first, second) {
-    return second[1] - first[1];
-  });
+    var total_num = (Object.keys(data['snitch_map']).length);
+    return total_num;
+  }
+ 
 
-  // Create a new array with only the first top_num items
-  var sorted_snitches_top = items.slice(0, top_num)
 
-  return sorted_snitches_top;
-}
- Vizu() {
+  Vizu() {
 
     // TODO: Things get fucked if there isn't already a data.json file in the data folder. FIX THIS
     // get an array of top tracking websites with the number of trackers that were snitching to that website
@@ -62,6 +71,7 @@ class Viz extends Component{
 
 render(){
   const v = this.get_top_trackers()
+  console.log(this.get_total_num_trackers())
     return (
       <div className="container" style={{ width: "600px" }}>
         <div className="my-3">
