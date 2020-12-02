436aca158f5f28d1edbc01069ba09c855ce2e5ee
ragnaag <45662914+ragnaag@users.noreply.github.com>
2020-12-01 20:10:09 -0500

    Changed to using another component for dropdown

--- src/Viz.js
+++ src/Viz.js
@@ -4,77 +4,19 @@ import * as d3 from 'd3';
 import Alert from 'react-bootstrap/Alert'
 import  BarChart from './BarChart';
 import  Bubble from './Bubble';
+import  Dropdown from './Dropdown';
 
 var data = require('./data/data.json');
 
+
 class Viz extends Component{
-  constructor() {
-    super();
 
-      this.state = {
-        showMenu: false,
-      }
-      this.closeMenu = this.closeMenu.bind(this);
-      this.showMenu = this.showMenu.bind(this);
-    }
-
-    showMenu(event) {
-      event.preventDefault();
-
-      this.setState({ showMenu: true }, () => {
-        document.addEventListener('click', this.closeMenu);
-      });
-    }
-
-    closeMenu() {
-
-       this.setState({ showMenu: false }, () => {
-         document.removeEventListener('click', this.closeMenu);
-       });
-
-     }
-
-
-  get_top_trackers() {
-
-  var snitches = {};
-
-  // Variable for Top __ websites with trackers on different websites
-  var top_num = 10
-
-  for (let tracker in data['snitch_map']) {
-    var websites = data['snitch_map'][tracker];
-    for (var i = 0; i < websites.length; i++) {
-      if (websites[i] in snitches){
-        snitches[websites[i]].push(tracker)
-      } else {
-        snitches[websites[i]] = [tracker];
-      }
-    }
+  get_total_num_trackers() {
+    // This function returns the total number of trackers based on the info stored in the snitch_map
+    // each key in the snitch map refers to the top level domain of a tracker
+    var total_num = (Object.keys(data['snitch_map']).length);
+    return total_num;
   }
-
-  // Create items array
-  var items = Object.keys(snitches).map(function(key) {
-    return [key, snitches[key]];
-  });
-
-  // Sort the array based on the second element
-  items.sort(function(first, second) {
-    return second[1].length - first[1].length;
-  });
-
-  // Create a new array with only the first top_num items
-  var sorted_snitches_top = items.slice(0, top_num)
-
-  return sorted_snitches_top;
-}
-
-get_total_num_trackers() {
-  // This function returns the total number of trackers based on the info stored in the snitch_map
-  // each key in the snitch map refers to the top level domain of a tracker
-  var total_num = (Object.keys(data['snitch_map']).length);
-  return total_num;
-}
 
     // TODO: Function to get the names of the trackers for these top websites
 
@@ -83,67 +25,53 @@ get_total_num_trackers() {
     // TODO: Function to get total number of allowed trackers
 
 
-render(){
-
-  const tracker_list = this.get_top_trackers()
-  console.log(tracker_list, " hi")
-
+  render(){
     return (
-
       <div className="container viz-container" style={{ width: "750px"}}>
         <div className="viz-page">
-        <h1 class="header">Let's breakdown your data!</h1><hr></hr><br></br>
-        <Alert variant='danger'>
-        We found a total of <b>{this.get_total_num_trackers()} trackers</b> throughout your browsing data.<br></br>
-        Each tracker comes from a different top-level-domain.
-        </Alert>
-        <br></br>
-        <h4 class="header" style={{marginBottom: 20}}>What websites track you the most?</h4>
-        <p>You probably have A LOT of trackers peeping your activity. We will first focus on the websites that track you the most.
+          <h1 class="header">Let's breakdown your data!</h1><hr></hr><br></br>
+          <Alert variant='danger'>
+          We found a total of <b>{this.get_total_num_trackers()}
+          trackers</b> throughout your browsing data.<br></br>
+          Each tracker comes from a different top-level-domain.
+          </Alert>
+          <br></br>
+          <h4 class="header" style={{marginBottom: 20}}>
+          What websites track you the most?</h4>
+          <p>You probably have A LOT of trackers peeping your activity.
+          We will first focus on the websites that track you the most.
 
-        <br></br><br></br>
+          <br></br><br></br>
 
-        The top 10 websites based on the number of trackers they had - </p>
-        <div><BarChart /></div>
+          The top 10 websites based on the number of trackers they had - </p>
+          <div><BarChart /></div>
 
-        <br></br><br></br>
+          <br></br><br></br>
 
-        <p>Next, it might be useful to look at the list of trackers for each of these top tracking websites.</p>
-        <h4 class="header" style={{marginBottom: 20}}>What trackers were on these websites?</h4>
+          <p>Next, it might be useful to look at the list of trackers for each
+          of these top tracking websites.</p>
+          <h4 class="header" style={{marginBottom: 20}}>
+          What trackers were on these websites?</h4>
 
-        <ul>
-        {tracker_list.map(datas =>
-            (<li>
-              <button class="btn btn-default dropdown-toggle"
-                data-toggle="dropdown"
-                type="button" style={{fontWeight: 600}}
-                onClick={this.showMenu}>
-                {datas[0]}
-              </button>
-              {this.state.showMenu ? (
-              <ul><li>{datas[1].map(line =>
-              (<li style={{listStyle: 'circle'}}>{line}</li>))}</li></ul>)
-              : ( null )}
-            </li>))}
-          </ul>
+          <div><Dropdown/> </div>
 
           <br></br>
           <hr></hr>
           <br></br>
-          <h4 class="header" style={{marginBottom: 20}}>Which trackers were most prevalent?</h4>
+          <h4 class="header" style={{marginBottom: 20}}>
+          Which trackers were most prevalent?</h4>
           <p>
           What about the prevalence of trackers? Which ones appear most in your browsing?
           The following bubble chart shows the top 10 trackers based on their prevalence in
           your browsing. Clicking the bubbles will take you to the website of these trackers
           where you can find more information.
           </p>
-
-          <Bubble/>
+        <div><Bubble/></div>
       </div>
       </div>
 
     );
-}
+  }
 }
 
 export default Viz;
